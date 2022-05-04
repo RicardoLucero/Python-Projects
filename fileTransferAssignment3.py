924b074868d76a0667635b21574019a9ea145a6a
Ricardo  Lucero <96747085+RicardoLucero@users.noreply.github.com>
2022-05-04 08:30:01 -0700

    corrected file transfer app
    able to correct a function that didn't allow to copy files over.

--- fileTransferAssignment3.py
+++ fileTransferAssignment3.py
@@ -3,7 +3,7 @@ import os
 import time
 from tkinter import *
 import tkinter.filedialog as fdialog
-
+from datetime import datetime, timedelta
 
 class MainClass():
 
@@ -29,18 +29,30 @@ class MainClass():
         buttonCopy.grid(row=11, column=3)
 
     SECONDS_IN_DAY = 24 * 60 * 60#sets the foundation to create a function
-    now = time.time()#that will look through the files and only copy the ones 
-    before = now - SECONDS_IN_DAY#that have been modified in the last 24 hrs
+    now = datetime.now()#that will look through the files and only copy the ones 
+    before = now - timedelta(hours=24)#that have been modified in the last 24 hrs
 
     def last_mod_time(fname):#here we are defining the function that will see the last
-        return os.path.getmtime(fname)#time the file was modified.
+        mtime = os.path.getmtime(fname)
+        modTime = datetime.fromtimestamp(mtime)
+        return modTime#time the file was modified.
 
-    def copyy(self):#this function will use the last_modified function and then use 
-        source_file = self.Source.get()#an if statement to use the parameters that will
-        dest_fold = self.Destination.get()#copy the files that were modified in the last 24 hrs and that they are .txt files
-        if (source_file.endswith(".txt") and last_mod_time(source_file) > before):
-            shutil.copy(source_file, dest_fold)
-            print("copy succesful")
+    def copyy(self):
+        #gets the path to the source folder
+        source_file = self.Source.get()
+        #gets the path to the destination folder
+        dest_fold = self.Destination.get()
+
+        #gets the list of files in the source path
+        fileList = os.listdir(source_file)
+
+        for file in fileList:
+            #gets the absolute path to the file in the source folder
+            absolutePath = os.path.join(source_file, file)
+            
+            if (absolutePath.endswith(".txt") and MainClass.last_mod_time(absolutePath) > MainClass.before):
+                shutil.copy(absolutePath, dest_fold)
+                print("copy succesful")
 
 def Source(self):
     source_file = self.Source.set(fdialog.askdirectory())
